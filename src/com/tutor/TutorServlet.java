421906ab2966bd945883f456e7b84cfa7b5fc65e
prasert <prasert_mx@hotmail.com>
2011-01-05 19:27:38 +0700

    ลองอีกที

---
+++ src/com/tutor/TutorServlet.java
@@ -0,0 +1,13 @@
+package com.tutor;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TutorServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
